a0bf20c6fba82804faca2fc0fc2bb3070602b406
Sophie Gillard <sophie.x.gillard@gmail.com>
2022-07-14 15:21:47 +0200

    end of exercice

---
+++ 01.js
@@ -0,0 +1,19 @@
+let htmlBadges = new Number('5');
+
+let cssBadges = new Number('7');
+
+let totalBadges = htmlBadges + cssBadges;
+
+console.log('Woaw, you have ' + totalBadges + "!");
+console.log("hi");
+
+console.log(12+7);
+
+var age= 22;
+console.log(age);
+
+var name= "Sophie";
+var firstName= "Gillard";
+var city= "Hevillers";
+
+console.log('My name is '+name+ firstName+' and I live in '+ city);
